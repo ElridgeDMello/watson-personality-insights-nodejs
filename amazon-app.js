0af7bb746c236802c8401f417f8c62c40e674931
Elridge D'Mello <elridge.dmello@drillinginfo.com>
2015-10-27 23:27:14 -0500

    Map response to a model that makes sense for our use case

--- amazon-app.js
+++ amazon-app.js
@@ -12,12 +12,23 @@ function getBooks(req, res) {
     var params = {
         keywords: req.params.keywords,
         searchIndex: 'Books',
-        responseGroup: 'Images,ItemAttributes,Offers',
+        responseGroup: 'Images,ItemAttributes',
         sort: 'relevancerank'
     };
 
     amazonClient.itemSearch(params).then(function(results) {
-        res.json(results);
+        var mappedResult = results.map(function (item) {
+            return {
+                url: item.DetailPageURL[0],
+                image: {
+                    url: item.LargeImage[0].URL[0],
+                    height: item.LargeImage[0].Height[0]._,
+                    width: item.LargeImage[0].Width[0]._
+                },
+                title: item.ItemAttributes[0].Title[0]
+            };
+        });
+        res.json(mappedResult);
     }).catch(function(err) {
         console.error(err);
         res.status(500).json({error: 'Something went wrong talking to Amazon!'});
