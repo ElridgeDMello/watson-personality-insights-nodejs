55700afc94edcba6b8c62177c3ca2394e29532f7
Elridge D'Mello <elridge.dmello@drillinginfo.com>
2015-10-27 22:27:39 -0500

    Convert to express service

--- amazon-app.js
+++ amazon-app.js
@@ -1,19 +1,32 @@
-var amazon = require('amazon-product-api');
+var amazon = require('amazon-product-api'),
+    app = require('express')();
 
-var client = amazon.createClient({
+var amazonClient = amazon.createClient({
     awsId: process.env.AMAZON_ASSOC_AWS_ID,
     awsSecret: process.env.AMAZON_ASSOC_AWS_SECRET,
     awsTag: process.env.AMAZON_ASSOC_AWS_TAG
 });
 
-client.itemSearch({
-    director: 'Quentin Tarantino',
-    actor: 'Samuel L. Jackson',
-    searchIndex: 'DVD',
-    audienceRating: 'R',
-    responseGroup: 'ItemAttributes,Offers,Images'
-}).then(function(results){
-    console.log(results);
-}).catch(function(err){
-    console.log(err);
-});
+function getBooks(req, res) {
+    "use strict";
+    var params = {
+        keywords: req.params.keywords,
+        searchIndex: 'Books',
+        responseGroup: 'Images,ItemAttributes,Offers',
+        sort: 'relevancerank'
+    };
+
+    amazonClient.itemSearch(params).then(function(results) {
+        res.json(results);
+    }).catch(function(err) {
+        console.error(err);
+        res.status(500).json({error: 'Something went wrong talking to Amazon!'});
+    });
+}
+
+app.get('/books/:keywords', getBooks);
+
+// start app listening
+var port = process.env.VCAP_APP_PORT || 3000;
+app.listen(port);
+console.log('listening at:', port);
